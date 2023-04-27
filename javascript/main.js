7eb07eeaf954df782b3856c46771d6178174957d
Eric Gonzalez <ericgonzadev@gmail.com>
2023-04-26 19:15:29 -0700

    Implement different method of initiating video header

--- javascript/main.js
+++ javascript/main.js
@@ -32,63 +32,63 @@
 			$('form').placeholder();
 
 		// Banner.
-			var $banner = $('#banner');
+			// var $banner = $('#banner');
 
-			if ($banner.length > 0) {
+			// if ($banner.length > 0) {
 
-				// IE fix.
-					if (skel.vars.IEVersion < 12) {
+			// 	// IE fix.
+			// 		if (skel.vars.IEVersion < 12) {
 
-						$window.on('resize', function() {
+			// 			$window.on('resize', function() {
 
-							var wh = $window.height() * 0.60,
-								bh = $banner.height();
+			// 				var wh = $window.height() * 0.60,
+			// 					bh = $banner.height();
 
-							$banner.css('height', 'auto');
+			// 				$banner.css('height', 'auto');
 
-							window.setTimeout(function() {
+			// 				window.setTimeout(function() {
 
-								if (bh < wh)
-									$banner.css('height', wh + 'px');
+			// 					if (bh < wh)
+			// 						$banner.css('height', wh + 'px');
 
-							}, 0);
+			// 				}, 0);
 
-						});
+			// 			});
 
-						$window.on('load', function() {
-							$window.triggerHandler('resize');
-						});
+			// 			$window.on('load', function() {
+			// 				$window.triggerHandler('resize');
+			// 			});
 
-					}
+			// 		}
 
-				// Video check.
-					var video = $banner.data('video');
+			// 	// Video check.
+			// 		var video = $banner.data('video');
 
-					if (video)
-						$window.on('load.banner', function() {
+			// 		if (video)
+			// 			$window.on('load.banner', function() {
 
-							// Disable banner load event (so it doesn't fire again).
-								$window.off('load.banner');
+			// 				// Disable banner load event (so it doesn't fire again).
+			// 					$window.off('load.banner');
 
-							// Append video if supported.
-								if (!skel.vars.mobile
-								&&	!skel.breakpoint('large').active
-								&&	skel.vars.IEVersion > 9)
-									$banner.append('<video autoplay loop><source src="public/banner.mp4" type="video/mp4" /><source src="public/banner.webm" type="video/webm" /></video>');
+			// 				// Append video if supported.
+			// 					if (!skel.vars.mobile
+			// 					&&	!skel.breakpoint('large').active
+			// 					&&	skel.vars.IEVersion > 9)
+			// 						$banner.append('<video autoplay loop poster="https://ryan-cho.s3.us-west-1.amazonaws.com/banner.jpg"><source src="public/banner.mp4" type="video/mp4" /><source src="public/banner.webm" type="video/webm" /></video>');
 
-                                // USE THIS IF USING AWS FOR VIDEO BANNER
-                                // if (!skel.vars.mobile
-                                // &&  !skel.breakpoint('large').active
-                                // &&  skel.vars.IEVersion > 9)
-                                //     $banner.append('<video autoplay loop><source src="https://ryan-cho.s3.us-west-1.amazonaws.com/banner.mp4" type="video/mp4" /><source src="https://ryan-cho.s3.us-west-1.amazonaws.com/banner..webm" type="video/webm" /></video>');
+            //                     // USE THIS IF USING AWS FOR VIDEO BANNER
+            //                     // if (!skel.vars.mobile
+            //                     // &&  !skel.breakpoint('large').active
+            //                     // &&  skel.vars.IEVersion > 9)
+            //                     //     $banner.append('<video autoplay loop><source src="https://ryan-cho.s3.us-west-1.amazonaws.com/banner.mp4" type="video/mp4" /><source src="https://ryan-cho.s3.us-west-1.amazonaws.com/banner..webm" type="video/webm" /></video>');
 
-						});
+			// 			});
 
-				// More button.
-					$banner.find('.more')
-						.addClass('scrolly');
+			// 	// More button.
+			// 		$banner.find('.more')
+			// 			.addClass('scrolly');
 
-			}
+			// }
 
 		// Scrolly.
 			$('.scrolly').scrolly();
